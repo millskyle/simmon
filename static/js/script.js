12aa2d3699ba16bcb53a267ac1566c67a8668ce8
Kyle Mills <kyle.mills@uoit.net>
2015-09-07 20:28:43 -0400

    fixed some more css issues

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+
+	// Initialize Masonry
+	$('#content').masonry({
+		columnWidth: 320,
+		itemSelector: '.item',
+		isFitWidth: true,
+		isAnimated: !Modernizr.csstransitions
+	}).imagesLoaded(function() {
+		$(this).masonry('reload');
+	});
+
+});
+
